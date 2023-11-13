f78dd37c3bac3e66225f8e75c080dd07e26c5904
dequevedo <danielelvisquevedo@gmail.com>
2023-11-12 23:52:32 -0300

    feat(POC): ControllerExceptionHandler implementation

--- src/main/java/com/pocspringboot/controller/ControllerExceptionHandler.java
+++ src/main/java/com/pocspringboot/controller/ControllerExceptionHandler.java
@@ -1,46 +1,51 @@
-package com.pocspringboot.controller;//package com.dequevedo.POCSpringBoot.controller;
-//
-//import com.dequevedo.POCSpringBoot.exception.NotFoundException;
-//import com.dequevedo.POCSpringBoot.exception.UnprocessableEntityException;
-//import com.dequevedo.POCSpringBoot.model.response.ErrorResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.context.MessageSource;
-//import org.springframework.context.i18n.LocaleContextHolder;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//@RequiredArgsConstructor
-//public class ControllerExceptionHandler {
-//
-//    private final MessageSource messageSource;
-//
-//    @ExceptionHandler(NotFoundException.class)
-//    public ErrorResponse notFoundExceptionHandler(Exception ex) {
-//        return ErrorResponse.builder()
-//                .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
-//                .message(getMessage("404.000"))
-//                .build();
-//    }
-//
-//    @ExceptionHandler(UnprocessableEntityException.class)
-//    public ErrorResponse unprocessableEntityExceptionHandler(Exception ex) {
-//        return ErrorResponse.builder()
-//                .code(String.valueOf(HttpStatus.UNPROCESSABLE_ENTITY.value()))
-//                .message(getMessage("422.000"))
-//                .build();
-//    }
-//
-//    @ExceptionHandler(Exception.class)
-//    public ErrorResponse globalExceptionHandler(Exception ex) {
-//        return ErrorResponse.builder()
-//                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-//                .message(getMessage("500.000"))
-//                .build();
-//    }
-//
-//    private String getMessage(String code, Object... args) {
-//        return this.messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
-//    }
-//}
+package com.pocspringboot.controller;
+
+import com.pocspringboot.exception.NotFoundException;
+import com.pocspringboot.exception.UnprocessableEntityException;
+import com.pocspringboot.model.response.ErrorResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Locale;
+
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class ControllerExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundExceptionHandler(Exception ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .message(getMessage("404.000"))
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(UnprocessableEntityException.class)
+    public ResponseEntity<ErrorResponse> unprocessableEntityExceptionHandler(Exception ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(String.valueOf(HttpStatus.UNPROCESSABLE_ENTITY.value()))
+                .message(getMessage("422.000"))
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> globalExceptionHandler(Exception ex) {
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+                .message(getMessage("500.000"))
+                .build();
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private String getMessage(String code, Object... args) {
+        return this.messageSource.getMessage(code, args, Locale.getDefault());
+    }
+}
