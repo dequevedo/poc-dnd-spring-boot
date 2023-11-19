bce0f7c3c09a3ac69a5980686f8d80f8829370a8
dequevedo <danielelvisquevedo@gmail.com>
2023-11-19 20:39:34 -0300

    feat(POC): Enum validation improvements

--- src/main/java/com/pocspringboot/controller/ControllerExceptionHandler.java
+++ src/main/java/com/pocspringboot/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.pocspringboot.controller;
 
+import com.pocspringboot.enumeration.MinMaxStrategyType;
 import com.pocspringboot.exception.NotFoundException;
 import com.pocspringboot.exception.StrategyNotFoundException;
 import com.pocspringboot.exception.UnprocessableEntityException;
@@ -7,12 +8,18 @@ import com.pocspringboot.model.response.ErrorResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.MessageSource;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 @Slf4j
 @RestControllerAdvice
@@ -24,9 +31,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> notFoundExceptionHandler(Exception ex) {
         log.error("NotFoundException: ", ex);
+        String code = "404.000";
+
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(String.valueOf(HttpStatus.NOT_FOUND.value()))
-                .message(getMessage("404.000"))
+                .code(code)
+                .message(getMessage(code))
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
@@ -34,9 +43,11 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(UnprocessableEntityException.class)
     public ResponseEntity<ErrorResponse> unprocessableEntityExceptionHandler(Exception ex) {
         log.error("UnprocessableEntityException: ", ex);
+        String code = "422.000";
+
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(String.valueOf(HttpStatus.UNPROCESSABLE_ENTITY.value()))
-                .message(getMessage("422.000"))
+                .code(code)
+                .message(getMessage(code))
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.UNPROCESSABLE_ENTITY);
     }
@@ -44,14 +55,37 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> globalExceptionHandler(Exception ex) {
         log.error("Exception: ", ex);
+        String code = "500.000";
+
         ErrorResponse errorResponse = ErrorResponse.builder()
-                .code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
-                .message(getMessage("500.000"))
+                .code(code)
+                .message(getMessage(code))
                 .build();
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    //TODO faz sentido ter um tratamento de exceção tão específico como esse aqui?
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        log.error("Exception: ", ex);
+        String code = "400.001";
+
+        List<String> fieldNames = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getField)
+                .toList();
+        List<String> allowedValues = Arrays.stream(MinMaxStrategyType.values())
+                .map(MinMaxStrategyType::getFieldName)
+                .toList();
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .code(code)
+                .message(getMessage(code, String.join(", ", fieldNames)))
+                .allowedValues(allowedValues)
+                .build();
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    //TODO faz sentido ter um tratamento de exceção tão específico como esse aqui nessa classe de handler?
     @ExceptionHandler(StrategyNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleStrategyNotFoundException(StrategyNotFoundException ex) {
         ErrorResponse errorResponse = ErrorResponse.builder()
--- src/main/java/com/pocspringboot/model/response/ErrorResponse.java
+++ src/main/java/com/pocspringboot/model/response/ErrorResponse.java
@@ -6,16 +6,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Builder
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
+
     private final String code;
+
     private final String message;
 
-    public ErrorResponse(String code, Object... args) {
+    private final List<String> allowedValues;
+
+    public ErrorResponse(String code, List<String> allowedValues, Object... args) {
         this.code = code;
         this.message = new MessageConfig().getMessage(code, args);
+        this.allowedValues = allowedValues;
     }
+
 }
